acb838e1a7af2dd0651e6dfcf296db91d61dcca5
kevin <ksgonzalez614@misena.edu.co>
2024-04-24 17:27:02 -0500

    cambiar la menera en la se llevan los datos en eventos con el array y se hizo creacion de la vista de los detalles del eventos y su direccionamiento y se crea direccionamiento a los pagos del evento por medio de el boton

--- src/main/java/DAO/eventoDAO.java
+++ src/main/java/DAO/eventoDAO.java
@@ -2,6 +2,7 @@ package DAO;
 
 import Modelo.conexion;
 import Modelo.cotizante;
+import Modelo.evento;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,6 +14,9 @@ import java.util.List;
  * @author kevin
  */
 public class eventoDAO {
+
+    evento evento = new evento();
+    cotizante cotizante = new cotizante();
 
     Connection con = new conexion().conectar();
     PreparedStatement ps;
@@ -27,17 +31,23 @@ public class eventoDAO {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                Object[] evento = new Object[5];
-                evento[0] = rs.getInt("idEvento");
-                evento[1] = rs.getString("Nombre_Cotizante");
+                evento.setEventoId(rs.getInt("idEvento"));
+                cotizante.setCotizanteNombre(rs.getString("Nombre_Cotizante"));
+                cotizante.setCotizanteApellido(rs.getString("Apellido_Cotizante"));
+                evento.setTipoEvento(rs.getString("Tipo_evento"));
+                evento.setEstadoEvento(rs.getString("Estado_evento"));
+                /*evento[1] = rs.getString("Nombre_Cotizante");
                 evento[2] = rs.getString("Apellido_Cotizante");
                 evento[3] = rs.getString("Tipo_evento");
-                evento[4] = rs.getString("Estado_evento"); // Dividir servicios concatenados en un array de Strings
-                lista.add(evento);
+                evento[4] = rs.getString("Estado_evento"); // Dividir servicios concatenados en un array de Strings*/
+                Object[] eventoArray = {evento.getEventoId(), cotizante.getCotizanteNombre(), cotizante.getCotizanteApellido(), evento.getTipoEvento(), evento.getEstadoEvento()};
+                lista.add(eventoArray);
             }
         } catch (Exception e) {
             e.printStackTrace(); // Imprimir el error
         }
         return lista;
     }
+    
+    
 }
