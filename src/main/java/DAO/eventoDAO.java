1f3ae9785e6a91532a089d7ea09b983e68a53d95
Felipe Rodriguez <dfelipebr737@gmail.com>
2024-06-17 20:09:59 -0500

    Se agrega la funcionalidad de enviar la encuesta de satisfaccion una vez se haya declarado el estado del evento como finalizado

--- src/main/java/DAO/eventoDAO.java
+++ src/main/java/DAO/eventoDAO.java
@@ -15,6 +15,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  *
@@ -31,7 +39,7 @@ public class eventoDAO {
     PreparedStatement ps;
     ResultSet rs;
     private static final String ESTADO_ACTIVO = "Activo";
-    
+
     public List<Object[]> listarEventos() {
         List<Object[]> lista = new ArrayList<>();
         String sql = "SELECT e.idEvento, c.Nombre_Cotizante, c.Apellido_Cotizante, e.Tipo_evento, e.Estado_evento FROM evento e "
@@ -85,7 +93,6 @@ public class eventoDAO {
                 String[] idServicio = rs.getString("idServicio").split(", ");
                 cliente.setCltId(rs.getString("idCliente"));
                 evento.setValorFinal(rs.getInt("Precio_Final"));
-                
 
                 if (nombresServicios.length == valorUnitario.length
                         && nombresServicios.length == cantidad.length
@@ -165,7 +172,7 @@ public class eventoDAO {
         return lista;
     }
 
-    public boolean actualizarEvento(evento evento) throws SQLException{
+    public boolean actualizarEvento(evento evento) throws SQLException {
         String sql = "CALL SP_UPDATE_EVENTO (?,?,?,?)";
         boolean exito = false;
         try {
@@ -174,22 +181,124 @@ public class eventoDAO {
             ps.setInt(2, evento.getValorEvento());
             ps.setString(3, evento.getEstadoEvento());
             ps.setString(4, evento.getDescripcionEvento());
-            
+
             int filasAfectadas = ps.executeUpdate();
-            
-            if(filasAfectadas > 0){
+
+            if (filasAfectadas > 0) {
                 exito = true;
-            
+
             }
-            
-        }catch(SQLException e){
+
+        } catch (SQLException e) {
             e.printStackTrace();
             throw e;
         }
         return exito;
     }
 
+    public List<Object[]> listarEventosParaEncuesta() throws SQLException {
+        List<Object[]> lista = new ArrayList<>();
+        String sql = "SELECT e.idEvento, c.Nombre_Cotizante, c.Apellido_Cotizante, e.Tipo_evento, e.Estado_evento, e.encuesta_enviada, c.Correo_Cotizante FROM evento e "
+                + "JOIN cliente cl ON e.Cliente_idCliente = cl.idCliente "
+                + "JOIN cotizante c ON cl.Correo_cotizante = c.Correo_Cotizante";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int idEvento = rs.getInt("idEvento");
+                String nombreCotizante = rs.getString("Nombre_Cotizante");
+                String apellidoCotizante = rs.getString("Apellido_Cotizante");
+                String tipoEvento = rs.getString("Tipo_evento");
+                String estadoEvento = rs.getString("Estado_evento");
+                boolean encuestaEnviada = rs.getBoolean("encuesta_enviada");
+                String correoCotizante = rs.getString("Correo_Cotizante");
+
+                if (estadoEvento.equals("Realizado") && !encuestaEnviada) {
+                    enviarCorreoEncuesta(idEvento, correoCotizante);
+                    marcarEncuestaComoEnviada(idEvento);
+                }
+
+                Object[] eventoArray = {idEvento, nombreCotizante, apellidoCotizante, tipoEvento, estadoEvento, encuestaEnviada};
+                lista.add(eventoArray);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return lista;
+    }
+
+    private void enviarCorreoEncuesta(int idEvento, String correoCliente) {
+        String encuestaLink = "https://forms.gle/7spyzDxBveciDuBE7";
+        String subject = "Encuesta de Satisfacción de Evento";
+        String content = "<p>Estimado cliente,</p>"
+                + "<p>Gracias por confiar en New Dreams para su evento. Por favor, complete la siguiente encuesta para ayudarnos a mejorar nuestros servicios:</p>"
+                + "<a href='" + encuestaLink + "'>Encuesta de Satisfacción</a>";
+        try {
+            sendEmail(correoCliente, subject, content);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            // Manejo de errores según sea necesario
+        }
+    }
+
+    private void marcarEncuestaComoEnviada(int idEvento) throws SQLException {
+        String sql = "UPDATE evento SET encuesta_enviada = TRUE WHERE idEvento = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, idEvento);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public static void sendEmail(String to, String subject, String content) throws MessagingException {
+        String host = "smtp.gmail.com"; // Cambia esto por el servidor SMTP que utilices
+        final String user = "gafeli29@gmail.com";
+        final String password = "rsqpprgqsgfgkyny";
+
+        // Configuración de las propiedades del servidor SMTP
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587"); // Cambia esto por el puerto que utilices
+
+        // Obtener la sesión de correo
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+
+        try {
+            // Crear un objeto MimeMessage
+            Message message = new MimeMessage(session);
+
+            // Configurar el remitente
+            message.setFrom(new InternetAddress(user));
+
+            // Configurar el destinatario
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+
+            // Configurar el asunto del correo
+            message.setSubject(subject);
+
+            // Configurar el contenido del correo
+            message.setContent(content, "text/html");
+
+            // Enviar el correo
+            Transport.send(message);
+
+            System.out.println("Correo enviado exitosamente");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
-    
-
-
